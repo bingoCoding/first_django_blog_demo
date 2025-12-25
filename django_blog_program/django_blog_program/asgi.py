aec46fac2d85b8c9994c5ac1b3aabe039e061dfb
supsky <>
2025-12-25 17:01:17 +0800

    demo init

---
+++ django_blog_program/django_blog_program/asgi.py
@@ -0,0 +1,17 @@
+"""
+ASGI config for django_blog_program project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+profile = os.environ.get('DJANGO_PROFILE', 'setting-dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_blog_program.settings.%s' % profile)
+
+application = get_asgi_application()
